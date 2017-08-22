eca62dd1cff5ac955d47e81c88644771dbc233c3
Jevgenij Pankov <eugene.pankov@gmail.com>
2017-08-22 20:59:25 +0300

    TemperatureSensor Visual Studio 2017 project
    Temperature Sensor project for Windows 10 IoT Core

---
+++ Temperature Sensor/TemperatureSensor/Controls/RadialMeter.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TemperatureSensor.Controls
+{
+    public sealed partial class RadialMeter : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value",
+            typeof(string),
+            typeof(RadialMeter),
+            new PropertyMetadata(default(string)));
+
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public RadialMeter()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ Temperature Sensor/TemperatureSensor/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Sensors.Dht;
+using System;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TemperatureSensor
+{
+    public sealed partial class MainPage : Page
+    {
+        private const int DHTPIN = 4;
+        private IDht dht = null;
+        private GpioPin dhtPin = null;
+        private DispatcherTimer sensorTimer = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            dhtPin = GpioController.GetDefault().OpenPin(DHTPIN, GpioSharingMode.Exclusive);
+            dht = new Dht11(dhtPin, GpioPinDriveMode.Input);
+            sensorTimer.Interval = TimeSpan.FromSeconds(1);
+            sensorTimer.Tick += sensorTimer_Tick;
+            sensorTimer.Start();
+
+            temperatureMeter.Value = "OFF";
+            humidityMeter.Value = "OFF";
+        }
+
+        private void sensorTimer_Tick(object sender, object e)
+        {
+            readSensor();
+        }
+
+        private async void readSensor()
+        {
+            double temp = 0;
+            double humidity = 0;
+
+            DhtReading reading = await dht.GetReadingAsync().AsTask();
+            if (reading.IsValid)
+            {
+                temp = reading.Temperature;
+                humidity = reading.Humidity;
+
+                temperatureMeter.Value = string.Format("{0:0.0}", temp);
+                humidityMeter.Value = string.Format("{0:0}", humidity);
+            }
+        }
+    }
+}
